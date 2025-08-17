a7a54f94e36187ec7cac5819d2bf3a3b5c4773ae
Nicolas Luna <nicolasluna.ca@gmail.com>
2025-08-16 22:53:54 -0300

    Update settings.py

--- back2fa/settings.py
+++ back2fa/settings.py
@@ -239,7 +239,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
     ],
 }
 
